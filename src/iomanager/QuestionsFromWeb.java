33751855fa1d84855551515a68a38fe45d9a5423
MrIrre <38958739+MrIrre@users.noreply.github.com>
2018-09-24 11:30:16 +0500

    Just.

---
+++ src/iomanager/QuestionsFromWeb.java
@@ -0,0 +1,64 @@
+package iomanager;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class QuestionsFromWeb {
+
+    public static Map<String, Set<String>> quizParser(int pageNumber) throws Exception {
+
+        Map<String, Set<String>> questionAndAnswer = new HashMap<>();
+        URL quizSite;
+        if (pageNumber == 0)
+            quizSite = new URL("https://baza-otvetov.ru/categories/view/1/");
+        else
+            quizSite = new URL("https://baza-otvetov.ru/categories/view/1/" + pageNumber + "0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(quizSite.openStream()));
+
+        String inputLine;
+        StringBuilder quizSiteText = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            quizSiteText.append(inputLine);
+        in.close();
+        String quizText = quizSiteText.toString();
+        String categories = quizText.substring(quizText.indexOf("q-list__table") + "q-list__table".length() + 2);
+        String cats = categories.substring(0, categories.indexOf("</table>"));
+
+        Pattern quotPattern = Pattern.compile("&quot;");
+        Matcher quotMatcher = quotPattern.matcher(cats);
+        cats = quotMatcher.replaceAll("");
+//        System.out.println(cats);
+
+        Pattern questionPattern = Pattern.compile("<a href.+?<\\/a>");
+        Matcher m = questionPattern.matcher(cats);
+
+        Pattern varianstsPattern = Pattern.compile("quiz-answers.+?<");
+        Matcher variantsMatcher = varianstsPattern.matcher(cats);
+
+        Pattern answerPattern = Pattern.compile("<td>\\D+?<\\/td>");
+        Matcher answerMatcher = answerPattern.matcher(cats);
+
+        while (m.find() && answerMatcher.find() && variantsMatcher.find()) {
+            String curLine = m.group();
+            String curVariants = variantsMatcher.group();
+            String curLineAns = answerMatcher.group();
+
+            String quest = curLine.substring(curLine.indexOf(">") + 1, curLine.lastIndexOf("<"));
+            String variants = curVariants.substring(curVariants.indexOf(":") + 2, curVariants.lastIndexOf("<"));
+            String ans = curLineAns.substring(curLineAns.indexOf(">") + 1, curLineAns.lastIndexOf("<"));
+
+            variants = variants.replaceAll("\\s", "");
+            ArrayList<String> variantsList = new ArrayList<>(Arrays.asList(variants.split(",")));
+            variantsList.add(ans);
+            Collections.shuffle(variantsList);
+//            System.out.println(quest + variantsList + "::" + ans);
+            Set<String> answerSet = new HashSet<>();
+            answerSet.add(ans.toLowerCase());
+            questionAndAnswer.put(quest + variantsList, answerSet);
+        }
+
+        return questionAndAnswer;
+    }
+}
---
+++ src/telegrambot/Bot.java
@@ -0,0 +1,41 @@
+package telegrambot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Сыграем?(Д\\Н)");
+        }
+    }
+
+    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            execute(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Quizzz...";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "635041031:AAGfEl36lQT8-KCSKNZZEQUPE87fGI5P1Ro";
+    }
+}
